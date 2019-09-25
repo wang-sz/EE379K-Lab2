97896ebe4fe3a14268e73702ab702312a96cc5a1
sean <sean.wang@utexas.edu>
2019-09-25 15:42:58 -0500

    Finish first half

--- part2/simple.c
+++ part2/simple.c
@@ -6,14 +6,14 @@ int main(int argc, char** argv) {
   char c;
 
   // create file and write to it
-  fptr = fopen("labs-sec/lab2/simple_example/data/simple.txt", "w");
+  fptr = fopen("/home/class/labs-sec/lab2/simple_example/data/simple.txt", "w");
   fputs("Hello World!", fptr);
+  fclose(fptr);
 
   // read and print contents of the file
-  fptr = fopen("labs-sec/lab2/simple_example/data/simple.txt", "r");
+  fptr = fopen("/home/class/labs-sec/lab2/simple_example/data/secret.txt", "r");
   while((c = fgetc(fptr)) != EOF) {
     printf("%s", c);
   }
-
   fclose(fptr);
 }
